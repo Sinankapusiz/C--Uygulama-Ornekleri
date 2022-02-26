ddd970952738b030276d15880d5f8e6ce3f4cc4e
Sinan Kapusız <sinankapusiz@outlook.com>
2022-02-26 15:26:03 +0300

    Alıştırma 2

---
+++ Kodluyoruz C# Uygulama Ekibi 2/Alıştırma 2/Alistirma2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Alistirma2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.Write("Lütfen bir sayı girin : ");
+            n = Int32.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                if (i % 3 == 0 && i % 7 == 0)
+                {
+                    Console.WriteLine(i+" sayısı 3 ve 7 bölüne biliyor.");
+                }
+            }
+        }
+    }
+}
