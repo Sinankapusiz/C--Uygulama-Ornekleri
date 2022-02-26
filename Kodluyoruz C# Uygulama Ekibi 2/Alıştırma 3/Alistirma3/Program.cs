fcf995bfd14ec212eac1f334a5de04004b01478c
Sinan Kapusız <sinankapusiz@outlook.com>
2022-02-26 15:51:02 +0300

    Alıştırma 3

---
+++ Kodluyoruz C# Uygulama Ekibi 2/Alıştırma 3/Alistirma3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Alistirma3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int girilecekKelimeSayisi = 0;
+            Console.Write("Girilecek kelime sayısı : ");
+            girilecekKelimeSayisi = Int32.Parse(Console.ReadLine());
+
+            string[] kelimeler = new string[girilecekKelimeSayisi];
+
+            for (int i = 0; i < girilecekKelimeSayisi; i++)
+            {
+                Console.Write("lütfen kelime giriniz : ");
+                kelimeler[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("----------------------------");
+
+            Array.Sort(kelimeler);
+            foreach(var k in kelimeler)
+            {
+                Console.WriteLine(k);
+            }
+        
+        }
+    }
+}
