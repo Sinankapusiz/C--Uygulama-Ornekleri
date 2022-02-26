d757d7bd90d5454e02d8d53cc959d827898f8f61
Sinan Kapusız <sinankapusiz@outlook.com>
2022-02-26 15:11:24 +0300

    Alıştırma 1

---
+++ Kodluyoruz C# Uygulama Ekibi 2/Alıştırma 1/Alistirma1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Alistirma1
+{
+    class Program //Author By Sinan Kapusız.
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 1000000; i++)
+            {
+                
+                bool asalSayi = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        asalSayi = false;
+                        break;
+                    }
+                }
+                if(asalSayi == true)
+                {
+                    Console.WriteLine(i+" Asal Sayı.");
+                }   
+            }
+        }
+    }
+}
