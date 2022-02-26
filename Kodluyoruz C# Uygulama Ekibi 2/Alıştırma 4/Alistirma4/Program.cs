b07cb22c92baf083b020bc078e8c56dff118aa32
Sinan Kapusız <sinankapusiz@outlook.com>
2022-02-26 16:04:39 +0300

    Alıştırma 4

---
+++ Kodluyoruz C# Uygulama Ekibi 2/Alıştırma 4/Alistirma4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Alistirma4
+{
+    class Program
+    {
+        static void Main(string[] args)//Author By Sinan Kapusız.
+        {
+            string metin;
+            Console.Write("Lütfen bir metin girin : ");
+            metin = Console.ReadLine();
+
+            string sesliHarfler =  "aeıioöuüAEIİOÖUÜ" ;
+
+            Console.Write("'"+metin+"' metnindeki sesli harfler : ");
+            for (int i = 0; i < metin.Length; i++)
+            {
+                for (int j = 0; j < sesliHarfler.Length; j++)
+                {
+                    if (metin[i] == sesliHarfler[j])
+                    Console.Write(sesliHarfler[j]+", ");
+                }
+            }
+        }
+    }
+}
