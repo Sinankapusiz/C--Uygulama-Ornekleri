2c9e23366b5fad73f164a915cb1ee8a034ddd7fb
Sinan Kapusız <sinankapusiz@outlook.com>
2022-02-26 16:51:24 +0300

    Alıştırma 7

---
+++ Kodluyoruz C# Uygulama Ekibi 2/Alıştırma 7/Alistirma7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Alistirma7
+{
+    class Program//Author by Sinan Kapusız.
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir metin girin : ");
+            string metin =Console.ReadLine();
+            Console.Write("Girdiğiniz metin kaç kere gösterilecek : ");
+            int gosterimSayisi=Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < gosterimSayisi; i++)
+            {
+                Console.WriteLine(metin);
+            }
+        }
+    }
+}
