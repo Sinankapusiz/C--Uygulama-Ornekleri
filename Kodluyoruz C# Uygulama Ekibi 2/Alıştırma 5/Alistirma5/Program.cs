b7531b86fad2c63d56e6e0024db13bff41c8e3ca
Sinan Kapusız <sinankapusiz@outlook.com>
2022-02-26 16:24:37 +0300

    Alıştırma 5

---
+++ Kodluyoruz C# Uygulama Ekibi 2/Alıştırma 5/Alistirma5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Alistirma5
+{
+    class Program//Author By Sinan Kapusız. 
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i+". Sayıyı giriniz : ");
+                toplam += Int32.Parse(Console.ReadLine()); 
+            }
+            Console.WriteLine("Girilen sayıların toplamı : "+toplam);
+        }
+    }
+}
