b395ebac58ed61136c77200872cabb313043c9c5
Sinan Kapusız <sinankapusiz@outlook.com>
2022-02-26 16:38:57 +0300

    Alıştırma 6

---
+++ Kodluyoruz C# Uygulama Ekibi 2/Alıştırma 6/Alistirma6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Alistirma6
+{
+    class Program//Author by Sinan Kapusız.
+    {
+        static void Main(string[] args)
+        {
+            int tek = 0, cift = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Random rastgeleSayi = new Random();
+                int sayi = rastgeleSayi.Next();
+                Console.WriteLine("Rastgele sayı tutulan sayı : " + sayi);
+                if (sayi % 2 == 0)
+                    cift++;
+                else
+                    tek++;
+            }
+            Console.WriteLine("Toplam çift sayı adeti : " + cift + " Toplam tek sayı adeti : " + tek);
+        }
+    }
+}
