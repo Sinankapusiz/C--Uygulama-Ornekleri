296e52fda7614480915b05ab0b5ca4062977afe4
Sinan Kapusız <sinankapusiz@outlook.com>
2022-03-06 21:08:39 +0300

    Metin içinde arama
    Verilen bir karakter dizininin substring() metodunu kullanarak string
    içerisinde arama yapan ve kaç defa geçtiğini bulan program.

---
+++ Kodluyoruz C# Uygulama Ekibi 3/Alıştırma 2/Alistirma2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Alistirma2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("lütfen bir metin girin : ");
+            string metin = Console.ReadLine();
+            Console.Write("Lütfen aranacak Karakteri girin : ");
+            char aranacakKarakter = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Karakterin bulunma sayısı : " + Substring(metin, aranacakKarakter));
+        }
+        public static int Substring(string metin, char aranacakKarakter)
+        {
+            int karakterinBulunmaSayisi = 0;
+            for (int i = 0; i < metin.Length; i++)
+            {
+                if (metin[i] == aranacakKarakter)
+                    karakterinBulunmaSayisi++;
+            }
+            return karakterinBulunmaSayisi;
+        }
+    }
+}
