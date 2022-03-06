f462790cdb25d82c9b8df6bac3a8b4bc26203717
Sinan Kapusız <sinankapusiz@outlook.com>
2022-03-06 20:56:26 +0300

    Girilen kelime sayısı
    Klavyeden girilen metnin kaç kelimeden oluştuğunu bulan program.

---
+++ Kodluyoruz C# Uygulama Ekibi 3/Alıştırma 1/Alistirma1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Alistirma1
+{
+    class Program//Author Sinan KAPUSIZ
+    {
+        static void Main(string[] args)
+        {
+            string metin;
+            Console.Write("Lütfen bir metin girin : ");
+            metin = Console.ReadLine();
+            string[] kelimeler = metin.Split(' ');
+            Console.WriteLine("Girdiğiniz metindeki toplam kelime sayısı : "+kelimeler.Length);
+        }
+    }
+}
