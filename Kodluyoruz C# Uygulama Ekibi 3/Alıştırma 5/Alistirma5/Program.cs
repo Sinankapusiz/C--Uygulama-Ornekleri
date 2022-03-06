42d83bdd30f3d27a9ec3b8547a6a6c60f8c02201
Sinan Kapusız <sinankapusiz@outlook.com>
2022-03-06 21:32:59 +0300

    Alıştırma 5
    1’den başlayıp 200’e kadar klavyeden girilen sayıya bölünen kaç adet sayı
    olduğunu veren program. (Örneğin klavyeden 6 girişi yapıldığında 1 ile 200 arasında 6’ya
    bölünen kaç adet sayı olduğunu ekrana yazdırması gerekir.)

--- Kodluyoruz C# Uygulama Ekibi 3/Alıştırma 5/Alistirma5/Program.cs
+++ Kodluyoruz C# Uygulama Ekibi 3/Alıştırma 5/Alistirma5/Program.cs
@@ -2,11 +2,23 @@
 
 namespace Alistirma5
 {
-    class Program
+    class Program//Author By Sinan KAPUSIZ.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Lütfen bir sayı girin : ");
+            int sayi = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(BolenSaysiniBul(sayi));
+        }
+        public static int BolenSaysiniBul(int sayi)
+        {
+            int bolunenSayiSayisi = 0;
+            for (int i = 1; i <= 200; i++)
+            {
+                if (i % sayi == 0)
+                    bolunenSayiSayisi++;
+            }
+            return bolunenSayiSayisi;
         }
     }
 }
