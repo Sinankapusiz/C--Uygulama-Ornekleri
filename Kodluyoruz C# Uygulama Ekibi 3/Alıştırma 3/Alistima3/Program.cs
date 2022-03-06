bc348a837fcd0e516da5b06b7f35303f3cf4a8b9
Sinan Kapusız <sinankapusiz@outlook.com>
2022-03-06 21:15:28 +0300

    Alıştırma 3
    Girilen sayı çift ise yarısını , tek ise 2 katını alarak ekrana yazdıran
    program.(Örneğin klavyeden 14 sayısı girilirse ekrana 7,11 sayısı girilirse ekrana 22
    yazılması gerekir.)

---
+++ Kodluyoruz C# Uygulama Ekibi 3/Alıştırma 3/Alistima3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Alistima3
+{
+    class Program//Author By Sinan KAPUSIZ.
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir Sayı girin : ");
+            int sayi =Int32.Parse(Console.ReadLine());
+            Console.WriteLine(sayi%2 == 0 ? sayi/2 : sayi * 2);
+        }
+    }
+}
