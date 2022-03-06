c820e4f10cab2375dae8c1657438174d19060137
Sinan Kapusız <sinankapusiz@outlook.com>
2022-03-06 21:25:34 +0300

    Alıştırma 4
    Klavyeden girilen bir sayının faktöriyelini alan program.

---
+++ Kodluyoruz C# Uygulama Ekibi 3/Alıştırma 4/Alistirma4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Alistirma4
+{
+    class Program//Author By Sinan KAPUSIZ.
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Faktöriyeli hesaplanacak sayıyı girin : ");
+            Console.WriteLine("Sonuç : " + FaktoriyeliniHesapla(Int32.Parse(Console.ReadLine())));
+            
+        }
+        public static int FaktoriyeliniHesapla(int sayi)
+        {
+            int fak = 1;
+            for (int i = 1; i <= sayi; i++)
+            {
+                fak *= i;
+            }
+            return fak;
+        }
+    }
+}
