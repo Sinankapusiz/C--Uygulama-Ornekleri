fa624ca542f1b33b222a04a8639ea05f3a312b4e
Sinan Kapusız <sinankapusiz@outlook.com>
2022-03-07 20:19:22 +0300

    Alıştırma 7
    Derece olarak verilen sıcaklığı Fahrenheita çeviren program

---
+++ Kodluyoruz C# Uygulama Ekibi 3/Alıştırma 7/Alistirma7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Alistirma7//Author By Sinan KAPUSIZ.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir sıcaklık derecesi girin : ");
+            int girilenDerece = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(girilenDerece + " girilen derece " + (girilenDerece * 1.8 + 32) + " Fahrenhayt.");
+        }
+    }
+}
