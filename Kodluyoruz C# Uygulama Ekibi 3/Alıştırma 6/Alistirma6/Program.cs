c674a7b857b9e43bd9672882592d7535b86c7c99
Sinan Kapusız <sinankapusiz@outlook.com>
2022-03-07 20:07:53 +0300

    Alıştırmalar 6
    Verilen bir kişi adını bir dizide arayan ve bulunup bulunamadığını belirten
    program. (Diziyi siz girebilirsiniz ya da kullanıcıdan alabilirsiniz.)

---
+++ Kodluyoruz C# Uygulama Ekibi 3/Alıştırma 6/Alistirma6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Alistirma6
+{
+    class Program//Author By Sinan KAPUSIZ.
+    {
+        //
+        static void Main(string[] args)
+        {
+            bool isimVar = false;
+            string[] isimler = {"osman","burak","mehmet","sinan","zafer","hakan","caner","volkan","süleyman","mehmet"};
+            Console.Write("Lütfen isminizi giriniz : ");
+            string aranacakIsim = Console.ReadLine();
+            aranacakIsim = aranacakIsim.ToLower();
+            foreach (var isim in isimler)
+            {
+                if (isim == aranacakIsim)
+                {
+                    Console.WriteLine("Bu "+aranacakIsim+", isimler dizisinde var.");
+                    isimVar = true;
+                }
+            }  
+            if (!isimVar)
+            {
+                Console.WriteLine("Bu "+aranacakIsim+", isimler dizisinde yok.");
+            }         
+        }
+    }
+}
