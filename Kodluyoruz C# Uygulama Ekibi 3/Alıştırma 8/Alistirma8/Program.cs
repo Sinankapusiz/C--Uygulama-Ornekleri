e337b25a93705ba2dc4b3e2f0543d8a85af47b48
Sinan Kapusız <sinankapusiz@outlook.com>
2022-03-07 20:28:06 +0300

    Alıştırma 8
    Girilen string ifadede boşluk karakterine kadar olan kısmı yazdıran
    program.(Örnek: Kodluyoruz’da yer alan bootcamp’lere başvuracağım cümlesi
    girildiğinde ekrana sadece Kodluyoruz’da yazdırılması gerekir.)

---
+++ Kodluyoruz C# Uygulama Ekibi 3/Alıştırma 8/Alistirma8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Alistirma8
+{
+    class Program//Author By Sinan Kapusız.
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir metin giriniz : ");
+            string[] metin = Console.ReadLine().Split(' ');
+            Console.WriteLine("Sonuç : " + metin[0]);
+        }
+    }
+}
